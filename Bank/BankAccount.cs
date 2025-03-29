2bbdfbdd3aea9f4822e6b597540127112ca8787d
Mohamed Melhem <melhemmohamed12@gmail.com>
2025-03-29 11:31:59 +0100

    BankAccount Kész.

---
+++ Bank/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal abstract class BankAccount : BankingService
+    {
+        double balance;
+        public BankAccount(Owner owner) : base(owner)
+        {
+        }
+
+        public double Balance { get => balance;}
+
+        public void Desposit(double amount)
+        {
+            balance += amount;
+        
+        }
+
+        public abstract bool Withdraw(double amount);
+
+
+
+    }
+}
---
+++ Bank/CreditAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class CreditAccount : BankAccount
+    {
+        double creditLimit;
+        public CreditAccount(double creditLimit,Owner owner) : base(owner)
+        {
+            this.creditLimit = creditLimit;
+        }
+
+        public double CreditLimit { get => creditLimit;}
+
+        public override bool Withdraw(double amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
