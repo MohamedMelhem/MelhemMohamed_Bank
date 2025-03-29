54ada435567e1e9af35fc4e19a09fe75edafb6cf
Mohamed Melhem <melhemmohamed12@gmail.com>
2025-03-29 12:28:17 +0100

    Bank Kész.

--- Bank/Bank.cs
+++ Bank/Bank.cs
@@ -8,11 +8,70 @@ namespace Bank
 {
     internal class Bank
     {
-        BankAccount[] accounts;
+        List<BankAccount> accounts;
 
-        public Bank(int count)
+        public Bank(int maxAccountCount)
         {
-            this.accounts = new BankAccount[count];
+            this.accounts = new List<BankAccount>(maxAccountCount);
         }
+
+        public BankAccount NewAccount(Owner owner, double creditlimit)
+        {
+            BankAccount account;
+            if (creditlimit > 0)
+            {
+                account = new CreditAccount(creditlimit, owner);
+            }
+            else
+            {
+                account = new SavingsAccount(owner);
+            }
+         
+            accounts.Add(account);
+            return account;
+        }
+
+        public double TotalBalance(Owner owner) 
+        { 
+            double total = 0;
+
+            foreach (BankAccount account in accounts) 
+            {
+                if (owner == account.Owner)
+                {
+                    total += account.Balance;
+                }
+
+            }
+            return total;
+            
+        
+        }
+        public BankAccount MaximalBalanceAccount(Owner owner) {
+            int index = 0;
+            for (int i = 0; i < accounts.Count; i++)
+            {
+                if (accounts[i].Owner == owner)
+                {
+                    if (accounts[i].Balance >= accounts[index].Balance)
+                    {
+                        index = i;
+                        
+                    }
+                }
+            }
+            return accounts [index];
+        }
+        public double TotalCreditLimit()
+        {
+            double totalCredit = 0;
+            foreach (CreditAccount account in accounts)
+            {
+                totalCredit += account.CreditLimit;
+
+            }
+            return totalCredit;
+        }
+
     }
 }
